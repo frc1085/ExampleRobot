115639790c32c4cc21069eeccf7c8b9078c44b15
Shmish <shmish90@gmail.com>
2017-11-10 20:12:58 -0500

    Added Framework

---
+++ src/main/kotlin/org/team9999/game/subsystems/ExampleSubsystem.kt
@@ -0,0 +1,13 @@
+package org.team9999.game.subsystems
+
+import edu.wpi.first.wpilibj.command.Subsystem
+import edu.wpi.first.wpilibj.command.TalonSRX
+
+class ExampleSubsystem : Subsystem () {
+
+  private val frontRight = TalonSRX(0)
+
+  override fun initDefaultCommand () {
+    setDefaultCommand(Command())
+  }
+}
